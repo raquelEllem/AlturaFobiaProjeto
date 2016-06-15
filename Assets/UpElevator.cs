edc8a79468364b6327484bdf1c8ba1410f0396ba
Raquel <raquel.ellem@gmail.com>
2016-06-15 12:14:47 -0300

    funcionando com dois andares - só que não desce do primeiro para o terreo

--- Assets/UpElevator.cs
+++ Assets/UpElevator.cs
@@ -7,16 +7,19 @@ public class UpElevator : MonoBehaviour
     public bool subindo;
     public bool descendo;
     public bool parado;
+    public bool um;
     public bool dois;
     public bool tres;
 
     public bool check;
 
+    public Transform terreo;
     public Transform primeiroAndar;
     public Transform segundoAndar;
     public Transform terceiroAndar;
 
     public float speed;
+    float aux;
           
 
     public GameObject botao;
@@ -29,46 +32,36 @@ public class UpElevator : MonoBehaviour
     {
 
         /***** DESCE ****/
+        // elevador desce para o terreo
+        if (descendo == true && um == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, terreo.transform.position, speed * Time.deltaTime);
+        }
+
+
         // elevador desce para o 1º andar
         if (descendo == true && dois == false)
         {
             transform.position = Vector3.MoveTowards(transform.position, primeiroAndar.transform.position, speed * Time.deltaTime);
-
         }
-
-
-        // elevador desce para o 2º andar
-        if (descendo == true && tres == false)
-        {
-            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, segundoAndar.transform.position.y, transform.position.z), speed * Time.deltaTime);
-        }
-
 
         /***** SOBE ****/
-        // elevador sobe para o 2º andar
-        if (subindo == true)
+        // elevador sobe para o 1º andar
+        if (subindo == true && um == true)
         {
-            //transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, 3, transform.position.z), speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, primeiroAndar.transform.position, speed * Time.deltaTime);
+        }
+
+        // elevador sobe para o 2º andar
+        if (subindo == true && dois == true)
+        {
             transform.position = Vector3.MoveTowards(transform.position, segundoAndar.transform.position, speed * Time.deltaTime);
         }
 
-        // elevador sobe para o 3º andar
-        if (subindo == true && dois == true)
-        {
-            //sobe muito devagar, quando começa a chegar perto do destino
-            //transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, terceiroAndar.transform.position.y, transform.position.z), speed * Time.deltaTime);
 
-            // sobe muito rápido por causa do 50f 
-            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, terceiroAndar.transform.position.y, transform.position.z), speed * 50f * Time.deltaTime);
-
-            //transform.position = Vector3.MoveTowards(transform.position, terceiroAndar.transform.position, speed * Time.deltaTime); 
-        }
-
-
-
-        /***** CONTROLA BOTÃO *****/ 
+        /***** CONTROLA BOTÃO *****/
         // se o elevador estiver parado o botão fica vermelho
-        if (transform.position == primeiroAndar.transform.position || transform.position == segundoAndar.transform.position || transform.position == terceiroAndar.transform.position)
+        if (transform.position == terreo.transform.position || transform.position == primeiroAndar.transform.position || transform.position == segundoAndar.transform.position || transform.position == terceiroAndar.transform.position)
         {
             botao.GetComponent<Renderer>().materials[0].color = Color.red;
             parado = true;
@@ -85,46 +78,49 @@ public class UpElevator : MonoBehaviour
 
 
         /***** TECLAS CLICADAS *****/
-        // se a tecla 'u' for apertada e o elevador estiver parado sobe
-        if (Input.GetKeyDown("u") && parado == true)
+        if (parado == true)
         {
-            //primeiro andar
-            if (transform.position == primeiroAndar.transform.position)
+            // se a tecla 'u' for apertada o elevador SOBE
+            if (Input.GetKeyDown("u"))
             {
                 subindo = true;
                 descendo = false;
+                // 1° andar
+                if (transform.position == terreo.transform.position)
+                {
+                    um = true;
+                }
+
+                // 2° andar
+                if (transform.position == primeiroAndar.transform.position)
+                {                    
+                    um = false;
+                    dois = true;
+                }
             }
 
-            //segundo andar
-            if (transform.position == segundoAndar.transform.position)
-            {
-                subindo = true;
-                descendo = false;
-                dois = true;
-            }
-        }
-
-        // se a tecla 'j' for apertada e o elevador estiver parado desce
-        if (Input.GetKeyDown("j") && parado == true)
-        {
-            //desce para o 1° andar
-            if (transform.position == segundoAndar.transform.position)
-            {
-                subindo = false;
-                descendo = true;
-                dois = false;
-            }
-
-            //desce para o 2° andar
-            if (transform.position == terceiroAndar.transform.position)
+            // se a tecla 'j' for apertada o elevador DESCE
+            if (Input.GetKeyDown("j"))
             {
                 subindo = false;
                 descendo = true;
-                tres = false;
-            }
-        }
+                
+                // terreo
+                if (transform.position == primeiroAndar.transform.position)
+                {
+                    um = false;
+                }
+
+                //1° andar
+                if (transform.position == segundoAndar.transform.position)
+                {
+                    dois = false;
+                }
+
+            }//fim da tecla
+
+        }//fim parado
     }
-
-  
 }
+
 
