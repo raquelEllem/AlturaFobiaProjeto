420d9d2899c5c30be1b6e01b27f7090aefee71d9
Raquel <raquel.ellem@gmail.com>
2016-06-22 14:45:34 -0300

    modelos de carro - script pronto, estão andando / animação, boneco andando tb funcionando

---
+++ Assets/Scripts/ElevadorSobeTempo.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevadorSobeTempo : MonoBehaviour
+{
+
+    // controla a velocidades do elevador
+    public float VelocidadeAceleracao;
+    public float VelocidadeAtual;
+    public float VelocidadeDesaceleracao;
+    public float VelocidadeMax;
+
+    float aux;
+    // controle
+    public bool subir;
+    public bool descer;
+    public bool parar;
+
+    public bool check;
+
+
+    public GameObject botao;
+    Collision colisao;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        subir = false;
+        parar = false;
+        descer = false;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        // se clicar no 'u' começa a subir
+        if (Input.GetKeyDown("e"))
+        {
+            subir = true;
+            parar = false;
+            descer = false;
+        }
+
+        // se clicar 'p' para a subida
+        if (Input.GetKeyDown("p"))
+        {
+            parar = true;
+            subir = false;
+            descer = false;
+        }
+
+        // se clicar 'l' desce
+        if (Input.GetKeyDown("l"))
+        {
+            descer = true;
+            parar = false;
+            subir = false;
+        }
+
+        //continua acelerando e subindo
+        if (subir == true)
+        {
+            transform.Translate(0, VelocidadeAtual, 0);
+            VelocidadeAtual = VelocidadeAtual + VelocidadeAceleracao / 10 * Time.deltaTime;
+            parar = false;
+        }
+        
+        //começa a descer
+        if (descer == true)
+        {
+            VelocidadeAtual = VelocidadeAtual + VelocidadeAceleracao / 10 * Time.deltaTime;
+            transform.Translate(0, -VelocidadeAtual, 0);
+        }
+
+        //começa a desacelerar e para
+        if (parar == true)
+        {
+            VelocidadeAtual = VelocidadeAtual - VelocidadeDesaceleracao * Time.deltaTime;
+            transform.Translate(0, 0, 0);
+        }
+
+        //limita a velocidade
+        if (VelocidadeAtual > VelocidadeMax)
+        {
+            VelocidadeAtual = VelocidadeMax;
+        }
+
+        //não deixa descer
+        if (VelocidadeAtual < 0)
+        {
+            VelocidadeAtual = 0;
+        }
+
+        /***** CONTROLA BOTÃO *****/
+        // se o elevador estiver subindo/descendo o botão fica verde
+        if (descer == true || subir == true)
+        {
+            botao.GetComponent<Renderer>().materials[0].color = Color.green;
+        }
+        else
+        {
+            //se o elevador estiver parado o botão fica vermelho
+            botao.GetComponent<Renderer>().materials[0].color = Color.red;
+        }
+
+        // não passar do chão
+        aux = transform.position.y;
+        if (aux < 0.022)
+        {
+       
+            parar = true;
+            descer = false;
+        }
+
+
+    }
+
+
+
+}
