0e96137afd946778cca709e55a9efd096f5bd6fe
Raquel <raquel.ellem@gmail.com>
2016-07-01 13:40:20 -0300

    script do carro - andando para frente

---
+++ Assets/Scripts/Carro.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Carro : MonoBehaviour {
+    
+    // controla as velocidades 
+    public float VelocidadeAtual;
+    public float VelocidadeAceleracao;
+    public float VelocidadeDesaceleracao;
+    public float VelocidadeMax;
+    float tempo;
+
+    bool andar;
+
+    // Use this for initialization
+    void Start () {
+
+        andar = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        tempo += Time.deltaTime;
+
+        if (tempo > 5)
+        {
+            andar = true;
+        }
+
+        if (andar == true)
+        {
+            VelocidadeAtual = VelocidadeAtual + VelocidadeAceleracao * Time.deltaTime;
+            transform.Translate(VelocidadeAtual, 0, 0);
+        }
+
+
+        //evitar que o carro vá para tras a qualquer momento
+        if (VelocidadeAtual < 0)
+        {
+            VelocidadeAtual = 0;
+        }
+
+    }
+}
