b86e93e14d1eca92e09803c5e165c01f10e0116a
Raquel <raquel.ellem@gmail.com>
2016-07-22 14:26:16 -0300

    carro andando até ponto determinado

---
+++ Assets/Scripts/AndarCarro.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndarCarro : MonoBehaviour {
+
+    public Transform Ponto;
+    public GameObject Carro1;
+    NavMeshAgent AuxPosicaoNavMesh;
+    Animation animacao;
+
+    public bool check;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        AuxPosicaoNavMesh = transform.GetComponent<NavMeshAgent>();
+
+        animacao = Carro1.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AuxPosicaoNavMesh.destination = Ponto.position;
+	}
+}
