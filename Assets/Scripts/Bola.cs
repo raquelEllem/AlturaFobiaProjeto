a283b90fc7164935683e76ba56d064429f6ad4ca
Raquel <raquel.ellem@gmail.com>
2016-07-26 10:58:46 -0300

    script para atirar

---
+++ Assets/Scripts/Bola.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bola : MonoBehaviour {
+
+    public Rigidbody rB;
+
+	// Use this for initialization
+	void Start () {
+
+        rB = transform.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //acelera a 9 m/s para frente
+        rB.velocity = transform.forward * 9;
+
+        //destroi bolas que não colidem com nada depois de 5 segundos
+        Destroy(gameObject, 5);
+
+        //se tiver qualquer coisa a 1 metro a frente do centro da bola (raycast)
+        // a bola é destruida
+        if (Physics.Raycast(transform.position, transform.forward, 1))
+        {
+            Destroy(gameObject);
+        }
+
+	}
+
+    void OnCollisionEnter (Collision colisao) {
+
+        //destroi bolas que colidem com alguma coisa
+        Destroy(gameObject);
+    }
+
+     
+}
+
